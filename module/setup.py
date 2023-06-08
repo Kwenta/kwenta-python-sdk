4e89342d3bdb1a6412642731e06a39597e0e4600
TossAyeCoin <60795666+TossAyeCoin@users.noreply.github.com>
2023-06-08 10:49:44 -0500

    Update setup.py
    updated version

--- module/setup.py
+++ module/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='kwenta',
-    version='1.0.4',
+    version='1.0.5',
     description='Python SDK for Kwenta',
     long_description='Python SDK for Kwenta',
     author='Kwenta DAO',
